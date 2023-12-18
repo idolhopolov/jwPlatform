aacf43c907218f96a9557f1cc7ab42d49e11040b
idolhopolov <krebtos@gmail.com>
2023-12-18 09:01:19 +0100

    architecture improvements

--- src/Application/JwPlatform/Service/ApiClient/JwPlatformApiClient.php
+++ src/Application/JwPlatform/Service/ApiClient/JwPlatformApiClient.php
@@ -8,12 +8,16 @@ use App\Application\Common\Service\ApiClient\AbstractCommonApiClient;
 use App\Application\JwPlatform\Service\ApiClient\Request\CreateProduct;
 use App\Application\JwPlatform\Service\ApiClient\Request\GetProduct;
 use App\Application\JwPlatform\Service\ApiClient\Response\JwPlatformApiClientResponse;
-use App\Application\JwPlatform\Service\ApiClient\Response\JwPlatformApiClientResponseInterface;
 use App\Application\Product\Command\Input\DTO\CreateProductPayload;
 use App\Application\Product\Query\Input\DTO\GetSingleProductInput;
 
-class JwPlatformApiClient extends AbstractCommonApiClient implements JwPlatformApiClientResponseInterface
+class JwPlatformApiClient extends AbstractCommonApiClient implements JwPlatformApiClientInterface
 {
+    public function __construct(JwPlatformApiClientConfiguration $configuration)
+    {
+        parent::__construct($configuration);
+    }
+
     public function fetchProduct(GetSingleProductInput $payload): JwPlatformApiClientResponse
     {
         return $this->call(new GetProduct($payload));
---
+++ src/Application/JwPlatform/Service/ApiClient/JwPlatformApiClientConfiguration.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Application\JwPlatform\Service\ApiClient;
+
+use App\Application\Common\Service\ApiClient\CommonConfigurationApiClientInterface;
+
+readonly class JwPlatformApiClientConfiguration implements CommonConfigurationApiClientInterface
+{
+    public function __construct(private string $jWPlatformApiHost, private string $jWPlatformApiToken)
+    {
+    }
+
+    public function getAuthHeader(): array
+    {
+        return ['X-Authorization' => $this->jWPlatformApiToken];
+    }
+
+    public function getBaseApiUrl(): string
+    {
+        return $this->jWPlatformApiHost;
+    }
+}
